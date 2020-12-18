d6397ac8e468a158c6e09a0594850f314d76bcac
EliasMoreira <eliasmatheus5088@gmail.com>
2020-12-18 08:25:30 -0300

    feat: novo layout

--- calculadora_Elias/app/src/main/java/com/example/calculadora_elias/MainActivity.kt
+++ calculadora_Elias/app/src/main/java/com/example/calculadora_elias/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.calculadora_elias
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextWatcher
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
